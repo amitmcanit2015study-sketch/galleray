a571e43168edf5e99f7cc9d79d78023cef8892b5
amitkatiyar <amit.katiyar@humanitics.com>
2026-09-02 00:15:58 +0530

    hgf

--- app/src/main/java/com/amitbharat/gallery/ui/activities/MainActivity.java
+++ app/src/main/java/com/amitbharat/gallery/ui/activities/MainActivity.java
@@ -123,6 +123,9 @@ public class MainActivity extends AppCompatActivity {
                 allMediaFragment.updateViewMode();
             }
             return true;
+        } else if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
         } else if (id == R.id.action_about) {
             startActivity(new Intent(this, AboutActivity.class));
             return true;
